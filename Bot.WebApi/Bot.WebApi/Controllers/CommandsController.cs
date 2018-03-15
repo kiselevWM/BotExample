796a8308e01cdd59e12a1b4edb3cfbe8405e385c
kiselev2620 <kiselevvv@webmoney.ru>
2018-03-15 11:29:41 +0300

    Проект web api

---
+++ Bot.WebApi/Bot.WebApi/Controllers/CommandsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using Bot.WebApi.Tools;
+using Newtonsoft.Json.Linq;
+
+namespace Bot.WebApi.Controllers
+{
+	public class CommandsController: ApiController
+	{
+		[HttpPost]
+		public async Task<IHttpActionResult> Exec([FromBody]JToken content)
+		{
+			var resp = string.Empty;// await _commandRequestProcessor.ProcessAsync(content.ToString());
+			return this.JsonString(resp);
+		}
+	}
+}
---
+++ Bot.WebApi/Bot.WebApi/Tools/JsonStringResultExtension.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Bot.WebApi.Tools
+{
+	public static class JsonStringResultExtension
+	{
+		public static CustomJsonStringResult JsonString(this ApiController controller, string jsonContent, HttpStatusCode statusCode = HttpStatusCode.OK)
+		{
+			var result = new CustomJsonStringResult(controller.Request, statusCode, jsonContent);
+			return result;
+		}
+
+		public class CustomJsonStringResult : IHttpActionResult
+		{
+			private readonly string _json;
+			private readonly HttpStatusCode _statusCode;
+			private readonly HttpRequestMessage _request;
+
+			public CustomJsonStringResult(HttpRequestMessage httpRequestMessage, HttpStatusCode statusCode = HttpStatusCode.OK, string json = "")
+			{
+				this._request = httpRequestMessage;
+				this._json = json;
+				this._statusCode = statusCode;
+			}
+
+			public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+			{
+				return Task.FromResult(Execute());
+			}
+
+			private HttpResponseMessage Execute()
+			{
+				var response = _request.CreateResponse(_statusCode);
+				response.Content = new StringContent(_json ?? "", Encoding.UTF8, "application/json");
+				return response;
+			}
+		}
+	}
+}
