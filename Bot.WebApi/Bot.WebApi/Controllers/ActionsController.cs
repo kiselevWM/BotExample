a9e0649dd64d0b58b7694862bd95c226285231f2
kiselev2620 <kiselevvv@webmoney.ru>
2018-03-15 12:26:48 +0300

    прослойка для кнопок

---
+++ Bot.WebApi/Bot.WebApi/Controllers/ActionsController.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using Bot.App.Actions.Test;
+using Bot.App.RequestProcessors.Actions;
+using Bot.WebApi.Tools;
+using Newtonsoft.Json.Linq;
+
+namespace Bot.WebApi.Controllers
+{
+	public class ActionsController: ApiController
+	{
+		[HttpPost]
+		public async Task<IHttpActionResult> Exec([FromBody] JToken content)
+		{
+			var resp = await new MainActionsRequestProcessor(new AuthService(), 
+				new ActionsRequestProcessor(new TestActionProcessor(), Properties.Settings.Default.Token)).ProcessAsync(content.ToString());
+			return this.JsonString(resp);
+		}
+	}
+}
